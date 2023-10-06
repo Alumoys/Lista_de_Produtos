9bf121ab65d18485c9b2af6dc2efec2087956020
Alumoys <128159774+Alumoys@users.noreply.github.com>
2023-10-06 12:03:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function pedir(){
+    var opcao = prompt("Escolha uma opção de 1 a 4")
+    switch(Number(opcao)){
+       
+        case 1: 
+        alert("Você escolheu lasanha")
+        break;
+        
+        case 2: 
+        alert("Você escolheu Coca-cola")
+        break;
+        
+        case 3: 
+        alert("Você escolheu Coxinha")
+        break;
+
+        case 4: 
+        alert("Você escolheu Joelho")
+        break;
+    }
+}
